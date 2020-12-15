06789733faae8f1208999bff312cd67b4518a7ac
Joe Fleming <joe.fleming@gmail.com>
2020-12-14 19:54:51 -0700

    feat: add world snapshot

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -1,11 +1,11 @@
-it('top 14', () => {
-  const viewWidth = 1280;
-  const viewHeight = 768;
-  const footerHeight = 129;
-  const paddingLeft = 96;
-  const paddingRight = 80;
-  const filename = (new Date()).toISOString()
+const viewWidth = 1280;
+const viewHeight = 768;
+const footerHeight = 129;
+const paddingLeft = 96;
+const paddingRight = 80;
+const filename = (new Date()).toISOString()
 
+it('US top 14', () => {
   // handle crappy websites
   cy.on('uncaught:exception', (err, runnable) => {
     // returning false here prevents Cypress from failing the test
@@ -23,7 +23,7 @@ it('top 14', () => {
 
   cy.get('#nav-yesterday .sorting:nth-of-type(4)').click()
 
-  cy.screenshot(filename, {
+  cy.screenshot(`${filename}-US`, {
     // capture: 'fullPage',
     capture: 'viewport',
     timeout: 10000,
@@ -31,14 +31,32 @@ it('top 14', () => {
       x: paddingLeft, y: 0, width: viewWidth - paddingLeft - paddingRight, height: viewHeight - footerHeight
     },
   })
+})
 
-  // log the top window's dimensions
-  const resolution = Cypress._.pick(top, [
-    'innerWidth',
-    'innerHeight'
-  ])
-  cy.task(
-    'log',
-    `top window inner w, h is ${resolution.innerWidth}x${resolution.innerHeight}`
-  )
+it('World top 14', () => {
+  // handle crappy websites
+  cy.on('uncaught:exception', (err, runnable) => {
+    // returning false here prevents Cypress from failing the test
+    return false
+  })
+
+  cy.visit('https://www.worldometers.info/coronavirus/')
+
+  // TODO: interact with the page to load results
+  cy.viewport(viewWidth, viewHeight)
+
+  cy.scrollTo(0, 1200)
+
+  cy.get('#nav-yesterday-tab a').click()
+
+  cy.get('#nav-yesterday .sorting:nth-of-type(4)').click()
+
+  cy.screenshot(`${filename}-World`, {
+    // capture: 'fullPage',
+    capture: 'viewport',
+    timeout: 10000,
+    clip: {
+      x: paddingLeft, y: 0, width: viewWidth - paddingLeft - paddingRight, height: viewHeight - footerHeight
+    },
+  })
 })
